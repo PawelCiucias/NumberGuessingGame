f40b60e25d680d9e0fe8fa011f55eb5319e22b0e
pav <pawel.ciucias@nestle.com>
2018-03-06 13:45:46 +0100

    solution to Exercise one

--- pc.NumberGame/Controllers/MessagesController.cs
+++ pc.NumberGame/Controllers/MessagesController.cs
@@ -17,18 +17,14 @@ namespace pc.NumberGame
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
             if (activity.Type == ActivityTypes.Message)
-            {
                 await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
-            }
             else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+                await HandleSystemMessage(activity);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private async Task<Activity> HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
@@ -37,9 +33,25 @@ namespace pc.NumberGame
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
+                // Handle conversation state changes, like members being added and removed Use Activity.MembersAdded and     
+                // Activity.MembersRemoved and Activity.Action for info Not available in all channels
+                if (message.MembersAdded.Count > 0)
+                {
+                    var connector = new ConnectorClient(new System.Uri(message.ServiceUrl));
+
+                    foreach (var member in message.MembersAdded)
+                    {
+                        // if the bot is added, then 
+                        if (member.Id == message.Recipient.Id)
+                        {
+                            var conversationId = message.Conversation.Id;
+                            var activityId = message.Id;
+                            var replyActivity = message.CreateReply("Hi I’m the Guess a number bot, please guess a number between 0 and 10.");
+
+                            await connector.Conversations.ReplyToActivityWithHttpMessagesAsync(conversationId, activityId, replyActivity);
+                        }
+                    }
+                }
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
--- pc.NumberGame/Dialogs/RootDialog.cs
+++ pc.NumberGame/Dialogs/RootDialog.cs
@@ -8,22 +8,51 @@ namespace pc.NumberGame.Dialogs
     [Serializable]
     public class RootDialog : IDialog<object>
     {
+        static Random rnd = new Random(DateTime.Now.Second * DateTime.Now.Millisecond);
+
+        int number = 0;
+        int count = 0;
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
-
+            number = rnd.Next(1, 11);
             return Task.CompletedTask;
         }
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var activity = await result as Activity;
+            count++;
+            int guess = -1;
 
-            // calculate something for us to return
-            int length = (activity.Text ?? string.Empty).Length;
+            var message = await result as IMessageActivity;
+            var response = context.MakeMessage();
+         
+            if (int.TryParse(message.Text, out guess) && guess > 0 && guess < 11)
+            {
+                if (guess < number)
+                    response.Text = $"your guess of '{guess}' was too low";
+                else if (guess > number)
+                    response.Text = $"your guess of '{guess}' was too high";
+                else
+                {
+                    response.Text = $"your guess of '{guess}' was correct, it took {count} guess's to get it right.\n\nFeel free to play again";
+                  
 
-            // return our reply to the user
-            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+                    //Pick next number to guess
+                    number = rnd.Next(1, 11);
+
+                    //reset counter
+                    count = 0;
+                }
+            }
+            else
+            {
+                response.Text = $"I am sorry '{ message.Text}' is not a valid integer between 1 and 10 inclusively";
+            }
+
+            await context.PostAsync(response);
 
             context.Wait(MessageReceivedAsync);
         }
